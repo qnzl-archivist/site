d6919c803f583b607c27df2e9067175c16e04d88
Madeline Cameron <maddie@qnzl.co>
2021-03-27 09:40:01 -0400

    chore(browser): Fix wording

--- browser/pages/login/html.js
+++ browser/pages/login/html.js
@@ -17,7 +17,7 @@ module.exports = (functions, state) => html`
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="mr-1 h-6 w-6">
               <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
             </svg>
-            QuantDash uses password-less auth, don't worry about passwords. :)
+            Archivist uses password-less auth, don't worry about passwords. :)
           </div>
           <div class="mt-2 text-sm">
             <div class="text-red-600 ${!state.login.error && `dn`}">
